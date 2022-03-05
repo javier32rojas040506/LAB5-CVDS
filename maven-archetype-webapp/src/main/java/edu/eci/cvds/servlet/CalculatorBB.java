2d21ee1d889c2a353f700e2d4c53ca380997c757
Francico Javier Rojass <javier32rojas040506@gmail.com>
2022-03-04 23:35:26 -0500

    ultimo punto

---
+++ maven-archetype-webapp/src/main/java/edu/eci/cvds/servlet/CalculatorBB.java
@@ -0,0 +1,135 @@
+package edu.eci.cvds.servlet;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+
+@ManagedBean(name = "calculatorBB")
+@SessionScoped
+public class CalculatorBB {
+    public ArrayList<Double> numbers;
+    public double mean;
+    public double mode;
+    public double standardDev;
+    public double variance;
+    public ArrayList<Double> numSubmited;
+
+    public CalculatorBB() {
+        numSubmited = new ArrayList<Double>();
+    }
+    public void setNumbers(String list) {
+        numbers = new ArrayList<Double>();
+        try {
+            String[] arrayString = list.split(";");
+            for (String stringNum:arrayString) {
+                numbers.add(Double.parseDouble(stringNum));
+            }
+            setNumSubmited(numbers);
+        }catch (Exception e)
+        {
+            System.out.println("se rompio");
+            System.out.println(e.getMessage());
+        }
+    }
+    public void setNumSubmited(ArrayList<Double> list){
+        for (Double num:list) {
+            if(!numSubmited.contains(num)) {
+                numSubmited.add(num);
+            }
+        }
+    }
+
+    public double getMean(){
+        return mean;
+    }
+
+    public void setMean(double mean) {
+        this.mean = mean;
+    }
+
+    public double getMode() {
+        return mode;
+    }
+
+    public void setMode(double mode) {
+        this.mode = mode;
+    }
+
+    public double getStandardDev() {
+        return standardDev;
+    }
+
+    public void setStandardDev(double standardDev) {
+        this.standardDev = standardDev;
+    }
+
+    public double getVariance() {
+        return variance;
+    }
+
+    public void setVariance(double variance) {
+        this.variance = variance;
+    }
+
+    public double calculateMean(String list){
+        setNumbers(list);
+        double partialMean = 0;
+        double sumMean = 0;
+        for (Double num: numbers){
+            sumMean += num;
+        }
+        partialMean = sumMean /(numbers.size()-1);
+        setMean(partialMean);
+        return partialMean;
+    }
+
+    public double calculateStandardDeviation(String list){
+        double variance =  calculateVariance(list);
+        double partialStandardDev = Math.sqrt(variance);
+        setStandardDev(partialStandardDev);
+        return partialStandardDev;
+    }
+
+    public double calculateVariance(String list){
+        double partialVariance = 0;
+        double mean = calculateMean(list);
+        double varianceSum = 0;
+        for (Double num: numbers) {
+            varianceSum += Math.pow(num-mean, 2f);
+        }
+        partialVariance = varianceSum / (numbers.size() -1);
+        setVariance(partialVariance);
+        return partialVariance;
+    }
+
+    public double calculateMode(String list){
+        int maxTimes = 0;
+        ArrayList<Double> duplicates= new ArrayList<Double>();
+        double partialMode = 0;
+        for (Double num: numbers) {
+            int times = 0;
+            for (Double num2: numbers) {
+                if(num.equals(num2)){
+                    times += 1;
+                }
+            }
+            if(times>maxTimes && times>1){
+                partialMode = num;
+                maxTimes = times;
+            }
+
+        }
+        setMode(partialMode);
+        return partialMode;
+    }
+
+    public void restart(){
+        numbers = new ArrayList<Double>();
+        numSubmited = new ArrayList<Double>();
+        setVariance(0);
+        setStandardDev(0);
+        setMean(0);
+        setMode(0);
+    }
+
+}
